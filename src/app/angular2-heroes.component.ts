e1c056569e188200ed360eb28e2973c49bbe9922
Zenon Zeniou <zzeniou86@gmail.com>
2016-05-24 16:38:24 +0100

    tutorial step 3

--- src/app/angular2-heroes.component.ts
+++ src/app/angular2-heroes.component.ts
@@ -1,32 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
-export /**
- * Hero
- */
-class Hero {
+export class Hero {
   id: number;
   name: string;
 }
 
 @Component({
-  moduleId: module.id,
   selector: 'angular2-heroes-app',
-  host: {
-    class: 'ui container'
-  },
   template: `
               <h3 class="ui dividing header">{{title}}</h3>
-              <h2>{{hero.name}} details!</h2>
-              <p><label class="ui horizontal label">id: </label>{{hero.id}}</p>
-              <p><label class="ui horizontal label">name: </label><input [(ngModel)]="hero.name" placeholder="name" class="ui input"></p>
+              <h2>My Heroes</h2>
+              <div class="ui relaxed list">
+                <div class="item" *ngFor="let hero of heroes"
+                (click)="onSelect(hero)">
+                  <div class="header">
+                    <a class="ui orange image label"
+                      [class.blue]="hero === selectedHero">
+                      {{hero.id}}
+                      <div class="detail">{{hero.name}}</div>
+                    </a>
+                </div>
+              </div>
+              <div *ngIf="selectedHero" class="ui compact blue segment">
+                <h2>{{selectedHero.name}} details!</h2>
+                <p><label class="ui horizontal label">id: </label>{{selectedHero.id}}</p>
+                <p><label class="ui horizontal label">name: </label>
+                  <input [(ngModel)]="selectedHero.name" placeholder="name" class="ui input">
+                </p>
+              </div>
             `
-  // templateUrl: 'angular2-heroes.component.html',
-  // styleUrls: ['angular2-heroes.component.css']
 })
 export class Angular2HeroesAppComponent {
+  @HostBinding('class') appClass = 'ui container';
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  selectedHero: Hero;
+  heroes = HEROES;
+  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
