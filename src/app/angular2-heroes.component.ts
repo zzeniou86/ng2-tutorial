5705a304597c2c8fb1136474309acc326f5a4418
Zenon Zeniou <zzeniou86@gmail.com>
2016-05-24 14:18:57 +0100

    tutorial step 2

--- src/app/angular2-heroes.component.ts
+++ src/app/angular2-heroes.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
 
+export /**
+ * Hero
+ */
+class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   moduleId: module.id,
   selector: 'angular2-heroes-app',
-  templateUrl: 'angular2-heroes.component.html',
-  styleUrls: ['angular2-heroes.component.css']
+  host: {
+    class: 'ui container'
+  },
+  template: `
+              <h3 class="ui dividing header">{{title}}</h3>
+              <h2>{{hero.name}} details!</h2>
+              <p><label class="ui horizontal label">id: </label>{{hero.id}}</p>
+              <p><label class="ui horizontal label">name: </label><input [(ngModel)]="hero.name" placeholder="name" class="ui input"></p>
+            `
+  // templateUrl: 'angular2-heroes.component.html',
+  // styleUrls: ['angular2-heroes.component.css']
 })
 export class Angular2HeroesAppComponent {
-  title = 'angular2-heroes works!';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
 }
